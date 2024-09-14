5e999c8b0e9eff42440ac17fb042cea4c73fdfc1
Lorraine-alt <lozanolorraine812@gmail.com>
2024-09-15 00:13:29 +0800

    Create strings.js

---
+++ strings.js
@@ -0,0 +1,8 @@
+let favActorFirstName = "Song"; 
+let favActorLastName = "Kang";  
+let fullName= favActorFirstName = "" + favActorLastName; 
+let uppercase = fullname.toUpperCase();
+let lowercase = "Britney".toLowerCase(); 
+let message = "My Favorite Actor is " + uppercase +. His Best show is Love Alarm."; 
+ 
+console.log(message);
